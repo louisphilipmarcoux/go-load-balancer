df9d77a29d27affe0af503f8657b7b35db1abe03
Louis-Philip Marcoux <louisphilipmarcoux@gmail.com>
2025-11-05 17:56:17 -0500

    complete stage 8 - add IP Hashing for sticky sessions (#6)

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"hash/fnv"
 	"io"
 	"log" // NEW: Import math
 	"net"
@@ -206,5 +207,69 @@ func TestBackendHealth(t *testing.T) {
 	b.SetHealth(false)
 	if b.IsHealthy() {
 		t.Error("Backend should be unhealthy after setting to false")
+	}
+}
+
+func TestIPHashing(t *testing.T) {
+	// 1. Setup test pool
+	b1 := &Backend{Addr: "server1"}
+	b2 := &Backend{Addr: "server2"}
+	b3 := &Backend{Addr: "server3"}
+	pool := &BackendPool{
+		backends: []*Backend{b1, b2, b3},
+	}
+
+	// 2. Set all as healthy
+	b1.SetHealth(true)
+	b2.SetHealth(true)
+	b3.SetHealth(true)
+
+	// 3. Define some test IPs and their known hash results
+	// We'll figure out which server they map to.
+	// We need a helper to know our expected index
+	getHashedIndex := func(ip string, count int) int {
+		h := fnv.New32a()
+		h.Write([]byte(ip))
+		return int(h.Sum32()) % count
+	}
+
+	ip1 := "192.168.1.1" // Client 1
+	ip2 := "10.0.0.5"    // Client 2
+	ip3 := "192.168.1.1" // Client 1 again
+
+	// 4. Test for stickiness
+	be1 := pool.GetNextBackendByIP(ip1)
+	be2 := pool.GetNextBackendByIP(ip2)
+	be3 := pool.GetNextBackendByIP(ip3)
+
+	// Test that the same IP (ip1 and ip3) gets the same backend
+	if be1.Addr != be3.Addr {
+		t.Errorf("IP Hashing failed: %s and %s got different backends (%s vs %s)",
+			ip1, ip3, be1.Addr, be3.Addr)
+	}
+
+	// 5. Test distribution (with our known inputs)
+	index1 := getHashedIndex(ip1, 3)
+	if be1.Addr != pool.backends[index1].Addr {
+		t.Errorf("IP %s was not mapped to the correct backend index %d", ip1, index1)
+	}
+	index2 := getHashedIndex(ip2, 3)
+	if be2.Addr != pool.backends[index2].Addr {
+		t.Errorf("IP %s was not mapped to the correct backend index %d", ip2, index2)
+	}
+
+	// 6. Test with an unhealthy backend
+	b2.SetHealth(false) // "server2" is now down
+
+	// Recalculate based on the *new* list of 2 healthy backends
+	healthyBackends := []*Backend{b1, b3}
+	index_ip1_unhealthy := getHashedIndex(ip1, 2) // Hash modulo 2
+
+	be_unhealthy := pool.GetNextBackendByIP(ip1)
+	expectedAddr := healthyBackends[index_ip1_unhealthy].Addr
+
+	if be_unhealthy.Addr != expectedAddr {
+		t.Errorf("IP Hashing with unhealthy backend failed. Expected %s, got %s",
+			expectedAddr, be_unhealthy.Addr)
 	}
 }
